0964e5a0e995d519e3946243143d514fe6af1e9d
brandonabeal110 <brandonabeal110@gmail.com>
2024-07-23 10:19:13 +0900

    added logic to notif page and added active modal notif
    Co-authored-by: kentaurse <kentaurse0212@gmail.com>
    Co-authored-by: brandonabeal110 <brandonabeal110@gmail.com>

--- src/components/NotificationListItem/NotificationListItem.jsx
+++ src/components/NotificationListItem/NotificationListItem.jsx
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Arrow from "../../assets/notifArrow.png";
+import ActiveNotificationModal from "../ActiveNotificationModal/ActiveNotificationModal";
 
-const NotificationListItem = () => {
+const NotificationListItem = ({ notificationData }) => {
+  const [isModalActive, setIsModalActive] = useState(false);
+  const [title, shortBody, fullBody] = [
+    notificationData.notificationTitle,
+    notificationData.notificationShortBody,
+    notificationData.notificationFullBody,
+  ];
+
+  function showModal() {
+    setIsModalActive(true);
+  }
+
   return (
     <div className="notif-news">
       <div className="notif-news-info">
-        <div className="notif-news-title">Brekfast</div>
-        <div className="notif-news-body">
-          Do not skip breakfast. Skipping breakfast will not help you lose
-          weight.
-        </div>
+        <div className="notif-news-title">{title}</div>
+        <div className="notif-news-body">{shortBody}</div>
       </div>
-      <div className="notif-news-icon">
+      <div className="notif-news-icon" onClick={showModal}>
         <img src={Arrow} alt="arrow" />
       </div>
+      <ActiveNotificationModal
+        title={title}
+        fullBody={fullBody}
+        setIsModalActive={setIsModalActive}
+        isModalActive={isModalActive}
+      />
     </div>
   );
 };
