200301990579f5081031d361a65d3a64aafa6eb5
zeusofsky <sibiriwolf@gmail.com>
2024-07-29 19:20:26 +0900

    changes

--- src/components/NotificationNewsList/NotificationNewsList.jsx
+++ src/components/NotificationNewsList/NotificationNewsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import useFetchUserNotifications from "../hooks/useFetchUserNotifications";
 import NotificationListItem from "../NotificationListItem/NotificationListItem";
 import "./notifNewsListStyle.css";
